eb29143eec40289b1dec757f4096d0591856bd8d
EnriqueAraujo <gilbertoaraujo229@gmail.com>
2020-01-17 13:10:09 -0300

    Matrizes CSharp

---
+++ DigitalInnovationOnePriemeiro/Programa.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace DigitalInnovationOnePriemeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  n;
+
+            Console.WriteLine("Criar matriz quadrada");
+            Console.WriteLine("-----------------------");
+
+            Console.WriteLine("Digite um valor para criar a matriz: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int [,]mat = new int [n,n];
+
+            
+        }
+    }
+}
